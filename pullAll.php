380390701272cb1015eddbc98fec1b405fc1806f
pejhmon <pejhmon@gmail.com>
2015-08-16 17:37:35 +0100

    access control fix

--- pullAll.php
+++ pullAll.php
@@ -3,14 +3,15 @@
 <?php
 	header("Access-Control-Allow-Origin: *");
 	// Encodes data from a POST request
-    $postdata = file_get_contents("php://input");
-    $request = json_decode($postdata);
+    // $postdata = file_get_contents("php://input");
+    // $request = json_decode($postdata);
+    $postdata = $_POST("tableName");
 
     //Uses said POST data
-    $table = $request->tableName;
+//    $table = $request->tableName;
 
     //Connects to the database and runs the query
-    $sql = "SELECT * FROM " . $table
+    $sql = "SELECT * FROM " . $postdata
     $result = sqlsrv_query($sql, $conn);
 
     //Saves the retrieved data as an array
