11bf7f3fdf72340e933a0b060fe8dde768286932
Pejh <pejhmon@users.noreply.github.com>
2015-08-31 17:22:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
+<?php include 'functions/shared_functions.php'; ?>
+
 <?php
+    header("Access-Control-Allow-Origin: *");
+    session_start();
+    // Encodes data from a POST request
+    $postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
+    //Uses said POST data
+    $check = $request->check;
+    
+    if ($check == "id"){
+        die(errorparse($_SESSION['id']));
+    }
 ?>
